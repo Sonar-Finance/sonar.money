c9114ef7ca471a98d735b2e8c7b2e149e9d31f10
D3Portillo <d3portillo@gmail.com>
2025-09-29 09:14:12 +0200

    chores: better mobile layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image"
 import Link from "next/link"
 
-import { FaArrowRight } from "react-icons/fa"
+import { FaTwitter, FaArrowRight, FaGithub } from "react-icons/fa"
 
 import asset_bg from "@/public/bg.webp"
 import asset_logo from "@/assets/logo.svg"
-import { FaTwitter } from "react-icons/fa6"
 
 export default function Home() {
   return (
@@ -30,6 +29,7 @@ export default function Home() {
 
             <Link
               target="_blank"
+              aria-label="Open Sonar Finance Mini App"
               href="https://world.org/mini-app?app_id=app_4bf83c14596fb3f11fbcd73f9686c8c1&draft_id=meta_fffef72e4142eb6e7ed8096590214c49"
               className="bg-white/10 border border-white/20 text-white px-4 py-2 rounded-lg font-semibold ring-2 ring-transparent hover:ring-white/25 transition"
             >
@@ -38,19 +38,21 @@ export default function Home() {
           </nav>
 
           <div className="text-white flex flex-col justify-center items-center text-center min-h-[60vh] mt-20">
-            <h1 className="text-4xl sm:text-7xl max-w-xl uppercase font-black">
-              Predictions. made <span className="underline">human</span>
+            <h1 className="text-4xl sm:text-7xl max-w-2xl uppercase font-black">
+              Predictions.
+              <br className="sm:hidden" /> made — human
             </h1>
             <p className="text-lg sm:text-xl mt-4 max-w-xl">
-              The human-powered prediction market on Worldchain. Turning
-              collective foresight into tradable truth.
+              <strong>Sonar:</strong> The human-powered prediction market on
+              Worldchain. Turning collective foresight into tradable truth.
             </p>
 
-            <nav className="mt-12 w-full sm:w-auto sm:mt-6 flex flex-col-reverse sm:flex-row gap-5">
+            <nav className="mt-12 w-full sm:w-auto sm:mt-6 flex flex-col sm:flex-row gap-5">
               <Link
                 target="_blank"
+                aria-label="Open Sonar Finance Mini App"
                 href="https://world.org/mini-app?app_id=app_4bf83c14596fb3f11fbcd73f9686c8c1&draft_id=meta_fffef72e4142eb6e7ed8096590214c49"
-                className="h-[3.75rem] group px-6 flex gap-3 text-black items-center justify-between font-semibold rounded-lg border-2 text-lg border-white/10 bg-gradient-to-bl from-sf-blue/90 to-sf-blue/60"
+                className="h-[3.75rem] group px-6 flex gap-4 sm:gap-3 text-black items-center justify-center font-semibold rounded-lg border-2 text-lg border-white/10 bg-gradient-to-bl from-sf-blue/90 to-sf-blue/60"
               >
                 <span>Start Predicting</span>
                 <FaArrowRight className="group-hover:translate-x-px" />
@@ -58,28 +60,40 @@ export default function Home() {
 
               <Link
                 target="_blank"
+                aria-label="Follow 0xSonarFinance on X (Twitter)"
                 href="https://x.com/0xSonarFinance"
-                className="h-[3.75rem] group px-6 flex gap-3 text-white items-center justify-between font-semibold rounded-lg border-2 text-lg border-white"
+                className="h-[3.75rem] group px-6 flex gap-3 text-white items-center justify-center font-semibold rounded-lg border-2 text-lg border-white"
               >
                 <span>Follow Twitter</span>
-                <FaArrowRight className="group-hover:translate-x-px sm:hidden" />
               </Link>
             </nav>
           </div>
 
-          <footer className="flex mt-4 py-2 items-center justify-center sm:justify-between">
-            <p className="text-center text-white/50 text-sm mt-20 mb-10">
+          <footer className="flex mt-44 sm:mt-20 gap-4 mb-8 flex-col sm:flex-row py-3 items-center justify-center sm:justify-between">
+            <p className="text-center text-white/50 text-sm">
               &copy; {new Date().getFullYear()} Sonar Finance. All rights
               reserved.
             </p>
 
-            <Link
-              className="hidden sm:block text-white/50 text-lg hover:text-white transition"
-              target="_blank"
-              href="https://x.com/0xSonarFinance"
-            >
-              <FaTwitter />
-            </Link>
+            <div className="flex items-center gap-4">
+              <Link
+                className="text-white/70 hover:text-white transition"
+                target="_blank"
+                aria-label="View source code on GitHub"
+                href="https://github.com/sonar-finance"
+              >
+                <FaGithub className="text-xl" />
+              </Link>
+
+              <Link
+                className="text-white/70 hover:text-white transition"
+                target="_blank"
+                aria-label="Follow 0xSonarFinance on X (Twitter)"
+                href="https://x.com/0xSonarFinance"
+              >
+                <FaTwitter className="text-xl" />
+              </Link>
+            </div>
           </footer>
         </section>
       </div>
